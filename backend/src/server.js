b45703a8bd4a55a35fae4b29c7c40242c35ea73b
eldugasa <eliasdugasa325@gmail.com>
2026-02-17 09:51:08 +0300

    I added  .env, server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
-// src/index.js (or wherever your entry file is)
+
 import dotenv from "dotenv";
 import app from "./app.js";
 
 dotenv.config();
-
+console.log("Current directory:", process.cwd());
+console.log("JWT_SECRET:", process.env.JWT_SECRET ? "✅ Loaded" : "❌ Not loaded");
+console.log("DATABASE_URL:", process.env.DATABASE_URL ? "✅ Loaded" : "❌ Not loaded");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
