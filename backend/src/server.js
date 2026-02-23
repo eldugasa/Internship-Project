06e445e38a3ffbd54fcefc93fb2855d18cf97133
eldugasa <eliasdugasa325@gmail.com>
2026-02-23 15:57:30 +0300

    I edited the sever.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,10 @@ console.log("Current directory:", process.cwd());
 console.log("JWT_SECRET:", process.env.JWT_SECRET ? "✅ Loaded" : "❌ Not loaded");
 console.log("DATABASE_URL:", process.env.DATABASE_URL ? "✅ Loaded" : "❌ Not loaded");
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://frontendprojecttaskmanagementsystem.vercel.app' // Replace with your actual frontend URL
+];
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
